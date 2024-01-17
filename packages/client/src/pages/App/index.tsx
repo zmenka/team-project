ddcb56e10c1f851b8894d10f40ab7e2fd1d42dcd
zmenka <zmenka345@gmail.com>
2024-01-17 11:44:02 +0100

    remove fetch from App

--- packages/client/src/pages/App/index.tsx
+++ packages/client/src/pages/App/index.tsx
@@ -1,18 +1,7 @@
-import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import './style.css'
 
 function App() {
-  useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
-
-    fetchServerData()
-  }, [])
   return (
     <div className="App">
       <Outlet />
